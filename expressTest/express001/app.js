b16ef4806c726f8abf33792f3471ffb26e273641
ide user ide_ezs <yakmoz@gmail.com>
2012-04-19 08:22:10 +0000

    express 의 여러모듈에 대한 테스트

--- expressTest/express001/app.js
+++ expressTest/express001/app.js
@@ -13,6 +13,8 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
+  app.use(express.cookieParser());      // 쿠키생성을 위한 미들웨어 추가
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -45,6 +47,34 @@ app.get('/product/insert',function(req,res){
 app.get('/product/edit',function(req,res){
     res.render('product/edit',{title:'Edit Page'});
 });
+
+app.get('/redirect',function(req,res) {
+    res.redirect('http://c9.io');
+});
+
+app.get('/cookie',function(req,res){
+    res.cookie('name1','value1',{expires:new Date(Date.now()+1000*60*10)});
+    res.cookie('name2','value2',{maxAge:1000*60*10});
+    
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end('<h1>'+JSON.stringify(req.cookies)+'</h1>');
+});
+
+app.get('/onlyChrome',function(req,res){
+   var agent = req.header('User-Agent');   
+   console.log('agent:'+agent);
+   
+   if(agent.toLowerCase().match(/chrome/)) {
+     res.send('<h1>welcome chrome...</h1>',{'Content-Type':'text/html'},200);
+   } else {
+     res.send('<h1>No chrome...</h1>',{'Content-Type':'text/html'},200);
+   }
+});
+
+app.get('/params',function(req,res){
+    res.send(req.param('id')+'/'+req.param('name'),{'Content-Type':'text/html'},200);
+});
+
 
 
 app.listen(process.env.C9_PORT, function(){
